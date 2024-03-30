bc0e2c0a249281b876e91b030e7a39decfd8b513
kudakwashe kwedza <kudakwashekwedza@gmail.com>
2024-03-30 08:45:48 +0000

    Resolved issue#1 corrected spelling error of word (please)

--- while.py
+++ while.py
@@ -1,13 +1,13 @@
 #create varibabes to store number
 num1 = 0 
 
-enter_num = int(input("plase Enter a Number (enetr -1 to exit):"))
+enter_num = int(input("Please Enter a Number (enetr -1 to exit):"))
 
 #create while loop
 while num1 != -1:
     num1 += enter_num  #same as num1 = num1 + enter_num
 
-    enter_num = int(input("plase Enter a Number(enter -1 to exit):"))
+    enter_num = int(input("Please Enter a Number(enter -1 to exit):"))
 
     if enter_num == -1:
       print(num1)
